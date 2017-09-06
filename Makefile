b235b292611b94be3f889c1f929b0f7051a54591
Hong Pin Alex Tan <alextan@administrators-MacBook-Pro.local>
2017-09-06 14:31:45 +0800

    chore: add setup for docker

---
+++ Makefile
@@ -0,0 +1,3 @@
+build:
+	echo "Building alextanhongpin/node-grpc"
+	docker build -t alextanhongpin/node-grpc .
--- dynamic/client.js
+++ dynamic/client.js
@@ -1,10 +1,13 @@
 const grpc = require('grpc')
 const path = require('path')
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 8080
 
 const transactionProto = grpc.load(path.join(__dirname, '../proto/transaction.proto')).transaction
+
+const url = 'http://localhost:4100/proto.GetTransaction/8080'
+// const url = `127.0.0.1:${port}`
 function main () {
-  const client = new transactionProto.Transaction(`localhost:${port}`, grpc.credentials.createInsecure())
+  const client = new transactionProto.Transaction(url, grpc.credentials.createInsecure())
 
   const call = client.getTransaction({ id: '123456690' })
   call.on('data', (item) => {
